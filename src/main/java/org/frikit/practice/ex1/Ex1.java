3ea8b44fa99926dc00747be503005d1e31a8c636
frikit <osipovvictor1994@gmail.com>
2019-12-15 16:29:32 +0000

    add first not clear exercise here to test output

--- src/main/java/org/frikit/practice/ex1/Ex1.java
+++ src/main/java/org/frikit/practice/ex1/Ex1.java
@@ -4,5 +4,14 @@ public class Ex1 {
 
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder(5);
+        String s = "";
+        if (sb.equals(s)) {
+            System.out.println("1");
+        } else if (sb.toString().equals(s)) {
+            //is 2 because 5 in the constructor of string builder is capacity not value and mean is empty == empty
+            System.out.println("2");
+        } else {
+            System.out.println("3");
+        }
     }
 }
