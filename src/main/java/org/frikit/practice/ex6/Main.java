1775ce521da7237d51e867b523ffd6d896ca66a6
frikit <osipovvictor1994@gmail.com>
2020-02-09 10:38:00 +0000

    add char int ex

---
+++ src/main/java/org/frikit/practice/ex6/Main.java
@@ -0,0 +1,13 @@
+package org.frikit.practice.ex6;
+
+public class Main {
+    public static void main(String[] args) {
+        char c;
+        int i;
+        c = 'a';
+        i = c;
+        i++;
+//        c = i; // int cannot fit in char this is why, my clue was correct
+        c++;
+    }
+}
