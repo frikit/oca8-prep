e47fe69e0253f28df030fce226876e36e045263f
frikit <osipovvictor1994@gmail.com>
2020-02-09 10:50:53 +0000

    add var declaration within method

---
+++ src/main/java/org/frikit/practice/ex7/Main.java
@@ -0,0 +1,10 @@
+package org.frikit.practice.ex7;
+
+public class Main {
+    public static void main(String[] args) {
+//        long i = 123_456_L; //cannot have _L can only have L
+//        long i = _123L; //cannot start with _
+//        float f = 123_.123f;// cannot have . after _
+        float f = 123_123F;// valid
+    }
+}
