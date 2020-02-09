310a5d3ce34ad02988a3b040ebe2b7b3dc0103e8
frikit <osipovvictor1994@gmail.com>
2020-02-09 12:28:42 +0000

    multiple assignment in java is valid ex

---
+++ src/main/java/org/frikit/practice/ex8/Main.java
@@ -0,0 +1,9 @@
+package org.frikit.practice.ex8;
+
+public class Main {
+    public static void main(String[] args) {
+        int i, j, k;
+        i = j = k = 9; //multiple assignment in one line is valid in java
+        System.out.println(i);
+    }
+}
